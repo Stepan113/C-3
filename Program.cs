85e5bad80bc2ed83df0230484b7a072d6836fa44
Ctepan <Ctepan>
2022-12-24 16:15:41 +0400

    19 task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num<100000 && num>-100000)
+{
+    if (num/10000==num%10 && num/1000%10==num%100/10)
+    {
+        Console.WriteLine($"Число {num} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
